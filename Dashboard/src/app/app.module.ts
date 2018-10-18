c9416f52f1867f75918ae745405ef3b8c3858b4d
Kristian Požgaj <kristianpozgaj@Kristians-MacBook-Pro.local>
2018-10-18 13:24:14 +0200

    add dashboard component

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
